6394f10bb82f2ec0ea3dc2f431a584e8b014a2cd
Alish Giri <alishgiri.92@gmail.com>
2022-11-07 13:59:37 +0545

    Create a home/landing page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import Container from "react-bootstrap/Container";
+
+const Home: React.FC = () => {
+  return (
+    <Container className="p-5">
+      <h2 className="text-center">Welcome to Dungeons & Dragons!</h2>
+      <h5 className="text-center">Spell Book</h5>
+    </Container>
+  );
+};
+
+export default Home;
