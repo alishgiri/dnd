89612022a3af984ededb93c3223b4d970ae02b0a
Alish Giri <alishgiri.92@gmail.com>
2022-11-07 13:47:29 +0545

    Create app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
+import Home from "./pages/Home";
 import AppBar from "./components/AppBar";
 import SpellsList from "./pages/SpellsList";
+import FavouriteList from "./pages/Favourites";
 
 function App() {
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AppBar />
       <Routes>
-        <Route path="/" element={<SpellsList />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/spells" element={<SpellsList />} />
+        <Route path="/favourites" element={<FavouriteList />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
