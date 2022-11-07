ebc4bcb5a7e6706e44f63853ad85313118113a2b
Alish Giri <alishgiri.92@gmail.com>
2022-11-07 13:46:54 +0545

    Create Dnd Services for API calls

--- src/services/DndService.ts
+++ src/services/DndService.ts
@@ -1,13 +1,50 @@
-import { Spells } from "../models/Spells";
 import axiosInstance from "./BaseSerivce";
+import { Spell, Spells } from "../models/Spells";
+
+let favouritesFromDatabase: Spell[] = [];
 
 const fetchSpells = async (): Promise<Spells> => {
   const res = await axiosInstance.get<Spells>("/spells");
+
+  // Highlight favourite items
+  if (favouritesFromDatabase.length > 0) {
+    favouritesFromDatabase.forEach((fav) => {
+      res.data.results.map((item) => {
+        if (item.name === fav.name) {
+          item.isFav = true;
+        }
+        return item;
+      });
+    });
+  }
+
   return res.data;
+};
+
+const fetchFavouriteSpells = async (): Promise<Spell[]> => {
+  // Make API request to get Favourite spells
+  return Promise.resolve(favouritesFromDatabase);
+};
+
+const addSpellToFav = async (spell: Spell): Promise<boolean> => {
+  // Call API to store the favourite spell
+  favouritesFromDatabase.push(spell);
+  return Promise.resolve(true);
+};
+
+const removeSpellFromFav = async (spell: Spell): Promise<boolean> => {
+  // Call API to remove the favourite spell
+  favouritesFromDatabase = favouritesFromDatabase.filter(
+    (s: Spell) => s.name !== spell.name
+  );
+  return Promise.resolve(true);
 };
 
 const DndService = {
   fetchSpells,
+  addSpellToFav,
+  removeSpellFromFav,
+  fetchFavouriteSpells,
 };
 
 export default DndService;
