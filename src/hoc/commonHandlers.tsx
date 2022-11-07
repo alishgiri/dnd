3575a349db2b487ab4317771dcef9ff1d9eaa535
Alish Giri <alishgiri.92@gmail.com>
2022-11-07 15:50:30 +0545

    Integrate spell detail modal on commonHandlers hoc

--- src/hoc/commonHandlers.tsx
+++ src/hoc/commonHandlers.tsx
@@ -1,15 +1,27 @@
 import React from "react";
 
+import { Spell } from "../models/Spells";
+import SpellDetailsModal from "../components/SpellDetailsModal";
+
 export interface CommonHandlerProps {
   errMsg: string | null;
+  toggleIsLoading: () => void;
   handleError: (error: any) => void;
+  toggleModal: (spell?: Spell) => void;
+}
+
+interface CommonHandlerState {
+  modalShow: boolean;
+  isLoading: boolean;
+  errMsg: string | null;
+  spell: Spell | null | undefined;
 }
 
 export default function commonHandlers(
   Component: React.ComponentType<CommonHandlerProps>
 ) {
-  return class extends React.Component {
-    state = { errMsg: null };
+  return class extends React.Component<{}, CommonHandlerState> {
+    state = { spell: null, errMsg: null, modalShow: false, isLoading: false };
 
     handleError = (error: any) => {
       if (typeof error === "string") {
@@ -21,15 +33,31 @@ export default function commonHandlers(
       }
     };
 
+    toggleModal = (spell?: Spell) => {
+      this.setState({ spell, modalShow: !this.state.modalShow });
+    };
+
+    toggleIsLoading = () => this.setState({ isLoading: !this.state.isLoading });
+
     render() {
-      const { errMsg } = this.state;
+      const { spell, errMsg, modalShow, isLoading } = this.state;
 
       return (
-        <Component
-          {...this.props}
-          errMsg={errMsg}
-          handleError={this.handleError}
-        />
+        <>
+          <Component
+            {...this.props}
+            errMsg={errMsg}
+            toggleModal={this.toggleModal}
+            handleError={this.handleError}
+            toggleIsLoading={this.toggleIsLoading}
+          />
+          <SpellDetailsModal
+            spell={spell}
+            show={modalShow}
+            isLoading={isLoading}
+            onHide={this.toggleModal}
+          />
+        </>
       );
     }
   };
